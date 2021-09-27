b888d6986cdc2ab755b3732f22acac524d90215e
erika <ermaks@ttu.ee>
2021-09-27 18:31:52 +0300

    added: ship module; ship services.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import {FormsModule,ReactiveFormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
+import { ShipModule } from './ship/ship.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+    ShipModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/ship/ship.ts
@@ -0,0 +1,10 @@
+export class Ship {
+  id: number;
+  name: string;
+  num_of_places: number;
+  constructor(id: number, name: string, num_of_places: number){
+    this.id = id;
+    this.name = name;
+    this.num_of_places = num_of_places;
+  }
+}
