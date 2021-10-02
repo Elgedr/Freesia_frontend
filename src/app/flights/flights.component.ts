e949b452c9ca8ea76191ae6a3fa9680845004c37
erika <ermaks@ttu.ee>
2021-10-02 21:39:31 +0300

    changed: style + removed unnecessary lines of code

--- src/app/flights/flights.component.ts
+++ src/app/flights/flights.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Flight } from "../flight";
 import {FLIGHTS} from "../mock-flights";
 
 @Component({
@@ -10,10 +9,6 @@ import {FLIGHTS} from "../mock-flights";
 })
 export class FlightsComponent implements OnInit {
   flights = FLIGHTS;
-  selectedFlight?: Flight;
-  onSelect(flight: Flight): void {
-    this.selectedFlight = flight;
-  }
 
   constructor() { }
 
