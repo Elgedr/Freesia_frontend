68a929c3dd40c671ca55dcc6219db52e346a985c
erika <ermaks@ttu.ee>
2021-10-02 20:39:24 +0300

    added: flight selector dropdown

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import {FormsModule,ReactiveFormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
-import { ShipModule } from './ship/ship.module';
+import { FlightsComponent } from './flights/flights.component';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from "@angular/material/input";
+import {MatSelectModule} from "@angular/material/select";
+
+import {BrowserAnimationsModule} from
+    '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
+    FlightsComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     AppRoutingModule,
-    ShipModule
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/flight.ts
@@ -0,0 +1,8 @@
+export interface Flight {
+  id: number;
+  ship_id: number;
+  departure_place: string;
+  departure_time: number;
+  destination_place: string;
+  arrival_time: number;
+}
