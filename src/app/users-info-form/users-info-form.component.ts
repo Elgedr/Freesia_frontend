0e0450e3782dac3515d9b5e3f4057a5bbd38fbb6
elgedr <elgedr@ttu.ee>
2021-10-13 22:05:10 +0300

    css for booked ticket was improved

--- src/app/users-info-form/users-info-form.component.ts
+++ src/app/users-info-form/users-info-form.component.ts
@@ -23,6 +23,7 @@ export class UsersInfoFormComponent implements OnInit {
     console.log('it does nothing', this.lname);
     console.log('it does nothing', this.phoneNumber);
     console.log('it does nothing', this.email);
+    // this.createPassenger =  new User (this.id, this.lname, this.fname, this.email, this.phoneNumber)
 
 
   }
