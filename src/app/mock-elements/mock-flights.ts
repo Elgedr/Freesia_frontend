500037280f3feeb643dd8b84e168dea87ae73f2d
Juliadjomina <judjom@ttu.ee>
2021-10-21 12:01:55 +0300

    flight mock deleted

--- src/app/mock-elements/mock-flights.ts
+++ src/app/mock-elements/mock-flights.ts
@@ -1,15 +1,10 @@
-// import {Flight} from "../models/flight";
-//
-// export const FLIGHTS: Flight[] = [
-//   {id: 0, shipId: 0, departurePlace: 'Tallinn', departureTime: 8.00, destinationPlace: 'Mars', arrivalTime: 14.00},
-//   {id: 1, shipId: 1, departurePlace: 'Washington', departureTime: 12.00, destinationPlace: 'Jupiter', arrivalTime: 23.00},
-//   {id: 2, shipId: 2, departurePlace: 'New-York', departureTime: 18.00, destinationPlace: 'Mars', arrivalTime: 19.00},
-//   {id: 3, shipId: 0, departurePlace: 'Stockholm', departureTime: 8.00, destinationPlace: 'Saturn', arrivalTime: 11.00},
-//   {id: 4, shipId: 1, departurePlace: 'Oslo', departureTime: 7.00, destinationPlace: 'Venus', arrivalTime: 14.00},
-//   {id: 5, shipId: 2, departurePlace: 'Helsinki', departureTime: 10.00, destinationPlace: 'Mars', arrivalTime: 21.00}
-// ];
+import {Flight} from "../models/flight";
 
-
-
-
-
+export const FLIGHTS: Flight[] = [
+  {id: 0, shipId: 0, departurePlace: 'Tallinn', departureTime: 8.00, destinationPlace: 'Mars', arrivalTime: 14.00},
+  {id: 1, shipId: 1, departurePlace: 'Washington', departureTime: 12.00, destinationPlace: 'Jupiter', arrivalTime: 23.00},
+  {id: 2, shipId: 2, departurePlace: 'New-York', departureTime: 18.00, destinationPlace: 'Mars', arrivalTime: 19.00},
+  {id: 3, shipId: 0, departurePlace: 'Stockholm', departureTime: 8.00, destinationPlace: 'Saturn', arrivalTime: 11.00},
+  {id: 4, shipId: 1, departurePlace: 'Oslo', departureTime: 7.00, destinationPlace: 'Venus', arrivalTime: 14.00},
+  {id: 5, shipId: 2, departurePlace: 'Helsinki', departureTime: 10.00, destinationPlace: 'Mars', arrivalTime: 21.00}
+];
