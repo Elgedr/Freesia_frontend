fbec644daee2c00c8219f657a8e3224d0c6366e3
Juliadjomina <judjom@ttu.ee>
2021-11-13 13:43:07 +0300

    firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   NASA_KEY: "88RZ8pMAIWZLf3n6wqKjFQmIZbUpSDcEInFHiRJo"
 };
 
