31c57b9de5c3ef2f08859394703f4fa75bae8928
Juliadjomina <judjom@ttu.ee>
2021-11-16 15:51:28 +0300

    url

--- src/app/services/flight.service.ts
+++ src/app/services/flight.service.ts
@@ -9,11 +9,11 @@ export class FlightService {
   }
 
   public getAllFlights(): Observable<any> {
-    console.log(this.http.get("http://localhost:8081/api/flights"));
-    return this.http.get("http://localhost:8081/api/flights");
+    console.log(this.http.get("http://localhost:8081/api/api/flights"));
+    return this.http.get("http://localhost:8081/api/api/flights");
   }
 
   public getFlightById(id: string): Observable<any> {
-    return this.http.get("http://localhost:8081/api/flights/" + id)
+    return this.http.get("http://localhost:8081/api/api/flights/" + id)
   }
 }
