fd3c439006acbb6ce41589644f5cdeae3ef5bf68
erika <ermaks@ttu.ee>
2021-12-22 20:19:54 +0200

    22/12: fixed the json in login/signup POST requests, now the user data actually goes through to the backend; fixed the incorrect auth api url; added a PROCEED button on frontend

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-const AUTH_API = 'http://localhost:8080/api/auth/';
+const AUTH_API = 'http://localhost:8081/api/auth/';
 
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
@@ -16,16 +16,16 @@ export class AuthService {
 
   login(username: string, password: string): Observable<any> {
     return this.http.post(AUTH_API + 'signin', {
-      username,
-      password
+      "username": username,
+      "password": password
     }, httpOptions);
   }
 
   register(username: string, email: string, password: string): Observable<any> {
     return this.http.post(AUTH_API + 'signup', {
-      username,
-      email,
-      password
+      "username": username,
+      "email": email,
+      "password": password
     }, httpOptions);
   }
 }
